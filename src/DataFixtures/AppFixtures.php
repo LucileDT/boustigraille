6ca882cfe163de3aaa1ba3c2a121ee072ea2608c
Yann-BUTSCHER-EIRL <yann.butscher.eirl@gmail.com>
2021-05-15 14:02:21 +0200

    Ref #42 : Added user fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,48 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Responsibility;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    /**
+     * Load the fixtures.
+     */
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // -- USERS -- //
 
+        /** @var Responsibility $roleAdmin */
+        $roleAdmin = $manager->getRepository(Responsibility::class)->find(1);
+
+        // Admin User
+        $admin = new User();
+        $admin->setUsername('admin');
+        $password = $this->encoder->encodePassword($admin, 'a');
+        $admin->setPassword($password);
+        $admin->addResponsibility($roleAdmin);
+
+        // Base user
+        $user = new User();
+        $user->setUsername('base_user');
+        $password = $this->encoder->encodePassword($user, '-+');
+        $user->setPassword($password);
+
+        $manager->persist($user);
+        $manager->persist($admin);
         $manager->flush();
+
+        // -- Ingredients -- //
     }
 }
