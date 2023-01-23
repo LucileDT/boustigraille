7310253731ee0bcdd39e06f179e68395fa02050b
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2023-01-23 18:40:12 +0100

    Ref #142: add forgotten shops

--- migrations/Version20230123124726.php
+++ migrations/Version20230123124726.php
@@ -21,7 +21,6 @@ final class Version20230123124726 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE ingredient DROP has_stock_check_needed_before_buying');
     }
 }
