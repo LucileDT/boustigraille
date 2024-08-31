8fd30b8256481441521c0241b473c3cf0a41dbef
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2024-08-31 21:14:26 +0200

    Ref #189 #107: update Symfony login method and fix top navbar redirection

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,17 +10,13 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils, UserRepository $userRepository): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        $currentUser = $this->getUser();
-
+        // get the login error if there is one
         $lastError = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
 
-        if (!is_null($currentUser))
-        {
-            return $this->redirectToRoute('home');
-        }
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
         if (!is_null($lastError))
         {
@@ -31,7 +26,7 @@ class SecurityController extends AbstractController
             }
             else
             {
-                if ($lastError->getMessage() == "An exception occurred in driver: SQLSTATE[HY000] [2002] Connection refused")
+                if ($lastError->getMessage() === 'An exception occurred in driver: SQLSTATE[HY000] [2002] Connection refused')
                 {
                     $this->addFlash('danger', 'Impossibilité de se connecter à la base de données.');
                 }
@@ -42,7 +37,9 @@ class SecurityController extends AbstractController
             }
         }
 
-        return $this->render('login/login.html.twig', []);
+        return $this->render('login/login.html.twig', [
+            'last_username' => $lastUsername,
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
