0cdf5397aabd096c482b4415c4967dc74e316e8e
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2024-09-09 00:03:08 +0200

    Ref #195: rework ingredient details page layout

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,7 @@ Encore
     .addEntry('meal_list_form', './assets/javascript/meal_list/_form.js')
     .addEntry('meal_list_card', './assets/javascript/meal_list/_meal_list_card.js')
     .addEntry('my_account_notifications', './assets/javascript/my_account/notifications.js')
+    .addEntry('ingredient_show', './assets/javascript/ingredient/show.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
