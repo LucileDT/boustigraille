30aecbd316d75a974bcb99157c58f2eca9dbcbfa
Yann-BUTSCHER-EIRL <yann.butscher.eirl@gmail.com>
2024-09-21 15:12:58 +0200

    Ref #207: fix NotificationSent mapping

--- src/Entity/NotificationSent.php
+++ src/Entity/NotificationSent.php
@@ -13,7 +13,7 @@ class NotificationSent
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\ManyToOne(inversedBy: 'notificationSents')]
+    #[ORM\ManyToOne(inversedBy: 'notificationsSent')]
     #[ORM\JoinColumn(nullable: true)]
     private ?User $sender = null;
 
